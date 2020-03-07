b30979621a9c64670c0e99d448fc979822b947a4
Vladilav Yepifanov <evn1999@yandex.ru>
2020-03-08 01:55:59 +0300

    comments about Sink/Source methods

--- src/main/kotlin/ru/spb/hse/nesh/interpreter/commands/io/IOInterfaces.kt
+++ src/main/kotlin/ru/spb/hse/nesh/interpreter/commands/io/IOInterfaces.kt
@@ -8,7 +8,13 @@ import java.io.OutputStream
  * Provides interface for a [ru.spb.hse.nesh.interpreter.commands.Command] to read from.
  */
 interface Source : Closeable {
+    /**
+     * Returns an input stream for command to use. Should return the same stream if called multiple times.
+     */
     fun getSourceStream(): InputStream
+    /**
+     * Returns a redirect for [ProcessBuilder] that may be used to get input from.
+     */
     fun getSourceRedirect(): ProcessBuilder.Redirect
 }
 
@@ -16,6 +22,12 @@ interface Source : Closeable {
  * Provides interface for a [ru.spb.hse.nesh.interpreter.commands.Command] to write to.
  */
 interface Sink : Closeable {
+    /**
+     * Returns an output stream for command to use. Should return the same stream if called multiple times.
+     */
     fun getSinkStream(): OutputStream
+    /**
+     * Returns a redirect for [ProcessBuilder] that may be used to put output.
+     */
     fun getSinkRedirect(): ProcessBuilder.Redirect
 }
